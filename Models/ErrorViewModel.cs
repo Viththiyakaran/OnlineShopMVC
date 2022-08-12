944736bc8d6e439232577d6bde5d6bed55054787
Viththiyakaran <battikaran6@gmail.com>
2022-08-12 06:11:33 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineStoreSara.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
