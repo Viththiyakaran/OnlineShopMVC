86905dc0cccea2025b3848cd61e38c1cc34c130f
Viththiyakaran <battikaran6@gmail.com>
2022-08-14 00:48:36 +0530

    Manufacturer model and controller

---
+++ Controllers/ManufacturerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreSara.Data;
+using OnlineStoreSara.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreSara.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ManufacturerController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Manufacturer> listData = _db.manufacturers.ToList();
+            return View(listData);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Manufacturer Obj)
+        {
+
+            if(ModelState.IsValid)
+            {
+                _db.manufacturers.Add(Obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Edit(int? Mid)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Models/Manufacturer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreSara.Models
+{
+    public class Manufacturer
+    {
+        [Key]
+        public int ManufacturerID { get; set; }
+        [Required]
+        [DisplayName("Manufacturer Name")]
+        public string ManufacturerName { get; set; }
+        [Required]
+        [DisplayName("Address")]
+        public string ManufacturerAddress { get; set; }
+        [Required]
+        [DisplayName("Contact Number ")]
+        public string ManufacturerContactNumber { get; set; }
+        [Required]
+        [DisplayName("Product Type ")]
+        public string ManufacturerProductType{ get; set; }
+
+        public bool ManufacturerIsActive { get; set; } = true;
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
