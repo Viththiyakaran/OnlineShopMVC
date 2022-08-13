47eb274b6bb3dbc35a826bc1313ca3515004c17c
Viththiyakaran <battikaran6@gmail.com>
2022-08-14 00:47:40 +0530

    AppDbContext Created

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStoreSara.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStoreSara.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Manufacturer> manufacturers { get; set; }
+
+       
+    }
+}
