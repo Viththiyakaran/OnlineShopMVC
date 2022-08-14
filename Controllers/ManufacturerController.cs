a6f3a37ec175460586b55fe412d00d888f2564f9
Viththiyakaran <battikaran6@gmail.com>
2022-08-14 12:02:20 +0530

    Manufacturer CURD done

--- Controllers/ManufacturerController.cs
+++ Controllers/ManufacturerController.cs
@@ -37,7 +37,7 @@ namespace OnlineStoreSara.Controllers
             {
                 _db.manufacturers.Add(Obj);
                 _db.SaveChanges();
-
+                TempData["Create"] = "Created New Successfully";
                 return RedirectToAction("Index");
             }
 
@@ -45,10 +45,80 @@ namespace OnlineStoreSara.Controllers
         }
 
 
-        public IActionResult Edit(int? Mid)
+        public IActionResult Edit(int? id)
         {
+            if( id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var listData = _db.manufacturers.Find(id);
+
+            if(listData == null)
+            {
+                return NotFound();
+            }
+
+            return View(listData);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Manufacturer Obj)
+        {
+           if(ModelState.IsValid)
+            {
+                _db.manufacturers.Update(Obj);
+                _db.SaveChanges();
+                TempData["Update"] = "Edit Item Successfully Done";
+                return RedirectToAction("Index");
+            }
+
             return View();
 
         }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var listData = _db.manufacturers.Find(id);
+
+            if (listData == null)
+            {
+                return NotFound();
+            }
+
+            return View(listData);
+
+        }
+
+
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var listData = _db.manufacturers.Find(id);
+
+            if (listData == null)
+            {
+                return NotFound();
+            }
+
+
+            _db.manufacturers.Remove(listData);
+            _db.SaveChanges();
+            TempData["Delete"] = "Delete has been done";
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
